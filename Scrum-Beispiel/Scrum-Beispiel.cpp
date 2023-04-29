585346fccd6e223e3f405fbc0b50a11198f6f88f
aschwaiger74 <78038701+aschwaiger74@users.noreply.github.com>
2023-04-29 18:55:01 +0200

    update

--- Scrum-Beispiel/Scrum-Beispiel.cpp
+++ Scrum-Beispiel/Scrum-Beispiel.cpp
@@ -2,13 +2,11 @@
 #include <iostream>
 using namespace std;
 
-int sum(int z1, int z2) {
-    return z1 + z2;
-}
+
 
 int main()
 {
     cout << "Hello World!" << endl;
-    cout << sum(13, 3) << endl;
+
 }
 
